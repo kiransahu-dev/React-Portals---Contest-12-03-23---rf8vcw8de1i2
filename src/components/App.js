c1c0ff86fb7eba50ec1cfd51c796ed3f6e647215
kiransahu-dev <61940769+kiransahu-dev@users.noreply.github.com>
2023-03-12 14:30:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,27 @@ import PortalTextArea from './PortalTextArea.js';
 
 //complete the function 
 const App = () => {
+  const [text, setText] = useState('');
+  
+  const handleChange = () => {
+    setText(event.target.value);
+  }
+  
+  const handleButton = () =>
+  {
+    ReactDom.render (
+    <portalTextArea text = {text} />,
+    document.getElementById('portal-textfield'));
+  }
 
 
 
   return (
     
     <div id="main">
-      <input id="input" value={} onChange={}></input><br/>
-      <PortalButton buttonclick={} /><br/>
-      <PortalTextArea value={}/>
+      <input id="input" value={text} onChange={handleButton}></input><br/>
+      <PortalButton buttonclick={handleButton} /><br/>
+      <div id ="portal-textfield"></div>
     </div>
   )
 }
