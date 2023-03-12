353c4c052e77b229420a1f256cc0832e4793f225
kiransahu-dev <61940769+kiransahu-dev@users.noreply.github.com>
2023-03-12 14:18:51 +0530

    created portal functionality

--- src/components/PortalButton.js
+++ src/components/PortalButton.js
@@ -2,13 +2,14 @@ import React from 'react';
 import  ReactDOM  from 'react-dom';
 
 //complete this function using portals
-const PortalButton=()=>{
-    return (
+const PortalButton=(props)=>{
+    return ReactDOm.createPortal(
         <div>
-            <button id="button" onClick={}>Click</button>
-        </div>
+            <button id="button" onClick={props.onClick}>Click</button>
+        </div>,
+        document.getElementById('portal-button')
         
-    )
+    );
 }
 export default PortalButton;
 
